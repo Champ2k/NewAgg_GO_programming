828a27226b9b7f012c498f30a7b23aeb46aa3f92
Champ2k <thehitmanza1@gmail.com>
2021-03-15 20:37:25 +0700

    fix xlm

--- gotut.go
+++ gotut.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"encoding/xml"
 	"io/ioutil"
+	"strings"
 )
 
 type NewsMap struct {
@@ -35,12 +36,13 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	var s Sitemapindex
 	var n News
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes, &s)
 	news_map := make(map[string]NewsMap)
 
 	for _, Location := range s.Locations {
+		Location = strings.Trim(Location, "\n")
 		resp, _ := http.Get(Location)
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		xml.Unmarshal(bytes, &n)
